dec776241e24dd0b82c33b35604176af7e928dbd
Keks <keks@htmlacademy.ru>
2023-11-09 19:42:22 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/render.js
@@ -0,0 +1,33 @@
+import { generatePics } from './data.js';
+import { showBigPicture } from './popup.js';
+
+const pics = generatePics();
+
+const picList = document.querySelector('.pictures');
+const picTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.createDocumentFragment();
+
+const renderPic = (picture) => {
+  const pictureElement = picTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__img').alt = picture.description;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  pictureElement.addEventListener('click', () => {
+    showBigPicture(picture);
+  });
+
+  return pictureElement;
+};
+
+
+const renderPics = () => {
+  pics.forEach((picture) => {
+    const pictureElement = renderPic(picture);
+    container.append(pictureElement);
+  });
+  picList.append(container);
+};
+
+export {renderPics};
+

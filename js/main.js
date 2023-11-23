fd788cb39bb6e806efa42460dd4e1ebc8ba3076b
kuujanis <sasha26x@yandex.ru>
2023-11-23 14:10:42 +0300

    module11-task1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
+// import { generatePics } from './data.js';
+import { loadData } from './fetch.js';
 import { renderPics } from './render.js';
+import { uplaodFormSubmit } from './form.js';
+import { showDataErrorMessage } from './status-messages.js';
 import './form.js';
 
-renderPics();
+let pics = [];
+
+const onSuccess = (data) => {
+  pics = data.slice();
+  renderPics(pics);
+  document.querySelector('.img-filters').classList.remove('img-filters--inactive');
+};
+
+loadData(onSuccess, showDataErrorMessage);
+
+uplaodFormSubmit();
+
+
